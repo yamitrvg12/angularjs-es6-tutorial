2b3bf50b965bd579db7d06ffc151c821e81e9fde
Yamit Villamil <yamit.villamil@globant.com>
2017-06-29 15:21:27 -0500

    Persisting JWT auth across page loads
    - Create a new service for saving, getting, and destroying a JWT token
    in localStorage.
    - Include the JWT service in services/index.js
    - Inject the JWT service and create a reference to it
    - Create the logout method in the User service.

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,9 +1,11 @@
 export default class User {
-    constructor(AppConstants, $http) {
+    constructor(AppConstants, $http, JWT, $state) {
         'ngInject';
 
         this._AppConstants = AppConstants;
         this._http = $http;
+        this._JWT = JWT;
+        this._$state = $state;
 
         // Object to store our user properties
         this.current = null;
@@ -21,8 +23,18 @@ export default class User {
         }).then(res => {
             // Store the user's info for easy lookup
             this.current = res.data.user;
-            
+
+            // Set the JWT token
+            this._JWT.save(res.data.user.token);
+
             return res;
         })
+    }
+
+    logout() {
+        this.current = null;
+        this._JWT.destroy();
+
+        this._$state.go(this._$state.current, null, {reload: true});
     }
 }
