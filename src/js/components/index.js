d8185a7492d5c0ae34e2c8d697bc94c555ab7e90
Yamit Villamil <yamit.villamil@globant.com>
2017-06-29 11:50:30 -0500

    Directives
    Creating a directive to show/hide an element depending on auth state

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -5,4 +5,7 @@ let componentsModule = angular.module('app.components', []);
 import ListErrors from './list-errors.component';
 componentsModule.component('listErrors', ListErrors)
 
+import ShowAuthed from './show-authed.directive';
+componentsModule.directive('showAuthed', ShowAuthed);
+
 export default componentsModule;
